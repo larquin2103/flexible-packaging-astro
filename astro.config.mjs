7e58173e884bed77f499c554daeb9f60ae0dfd87
larquin2103 <56054193+larquin2103@users.noreply.github.com>
2025-10-23 21:35:30 -0400

    Add particles integration and update site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
+import particles from '@astrojs/particles'
 
 export default defineConfig({
-  integrations: [react(), tailwind(), sitemap()],
-  site: 'https://tufabricaenvases.com',
+  integrations: [react(), tailwind(), sitemap(), particles()],
+  site: 'https://fabrica-envases-flexibles.netlify.app',
   output: 'static',
 })
