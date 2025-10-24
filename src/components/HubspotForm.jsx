dabff21fb2c6aa9f911e2751383316f6b107e7d7
larquin2103 <56054193+larquin2103@users.noreply.github.com>
2025-10-23 20:49:34 -0400

    Add HubspotForm component to embed HubSpot form

---
+++ src/components/HubspotForm.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from 'react'
+
+export default function HubspotForm() {
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.src = 'https://js.hsforms.net/forms/embed/v2.js'
+    script.onload = () => {
+      if (window.hbspt) {
+        window.hbspt.forms.create({
+          portalId: import.meta.env.PUBLIC_HUBSPOT_PORTAL_ID,
+          formId: import.meta.env.PUBLIC_HUBSPOT_FORM_ID,
+          target: '#hubspot-form',
+        })
+      }
+    }
+    document.body.appendChild(script)
+  }, [])
+
+  return <div id="hubspot-form" class="max-w-xl mx-auto" />
+}
