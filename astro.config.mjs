87ed1bbaaff64f1228790edae2a74b53f1d2fdd5
larquin2103 <56054193+larquin2103@users.noreply.github.com>
2025-10-23 20:42:33 -0400

    Add Astro configuration with React and Tailwind

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
+
+export default defineConfig({
+  integrations: [react(), tailwind(), sitemap()],
+  site: 'https://tufabricaenvases.com',
+  output: 'static',
+})
