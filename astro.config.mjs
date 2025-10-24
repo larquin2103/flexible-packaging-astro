8ff9a6f9330e343a7daaa6a81ba76c3d84d54caf
larquin2103 <56054193+larquin2103@users.noreply.github.com>
2025-10-23 21:54:47 -0400

    Change particles integration import path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
-import particles from '@astrojs/particles'
+import particles from 'astro-particles'  // ← cambio aquí
 
 export default defineConfig({
   integrations: [react(), tailwind(), sitemap(), particles()],
