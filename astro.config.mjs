e32da4b9637378ceb1d095225eadff1304c5e0fd
larquin2103 <larquin2103new@gmail.com>
2025-10-27 14:10:21 -0400

    modificando packa.joson

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
-import particles from '@astrojs/particles'
 
 export default defineConfig({
-  integrations: [react(), tailwind(), sitemap(), particles()],
+  integrations: [react(), tailwind(), sitemap()],
   site: 'https://fabrica-envases-flexibles.netlify.app',
   output: 'static',
 })
