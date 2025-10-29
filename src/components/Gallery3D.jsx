68b096e137786a47fafcab7802d72088afd9d50f
larquin2103 <larquin2103new@gmail.com>
2025-10-29 10:19:12 -0400

    personalizando la descarga  de las fichas tecnicas

--- src/components/Gallery3D.jsx
+++ src/components/Gallery3D.jsx
@@ -56,13 +56,13 @@ export default function Gallery3D() {
             <h3 className="text-2xl font-bold mb-2">{productos[selected].name}</h3>
             <p className="text-gray-700 mb-4">{productos[selected].desc}</p>
             <div className="flex justify-center gap-4">
-              <a
+              {/*<a
                 href={`/fichas/${productos[selected].name.toLowerCase().replace(/ /g, '-')}.pdf`}
                 download
                 className="bg-blue-600 text-white px-5 py-2 rounded-full hover:bg-blue-700 transition"
               >
                 Descargar Ficha
-              </a>
+              </a>*/}
               <a href="/#cotizar" class="bg-blue-600 text-white px-6 py-3 rounded-full font-bold hover:bg-blue-700 transition">
                 Solicitar Cotización
               </a>
